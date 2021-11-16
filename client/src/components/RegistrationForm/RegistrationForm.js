1eee5db1c435051511be51d4fcd6b7fe5d0f8a45
Annastiina Ahola <anna.s.ahola@gmail.com>
2021-11-16 08:54:58 +0200

    Implement basic registration frontend
    Implement some basic username and password validation (being required, length limits). Implement an error message if the user manages to try to submit inputs that don't meet the validation requirements. Implement sending a POST request with the credentials to 'http://localhost:5000/register'.

--- client/src/components/RegistrationForm/RegistrationForm.js
+++ client/src/components/RegistrationForm/RegistrationForm.js
@@ -1,34 +1,131 @@
 import React, { Fragment } from "react";
 import "./RegistrationForm.css";
 
-const RegistrationForm = () => {
-  return (
-    <Fragment>
-        <div className="registration-container">
-            <form>
-                <h1>Register</h1>
-                <div className="form-group">
-                    <label htmlFor="registrationUsernameInput"><h5>Username</h5></label>
-                    <div className="input-group">
-                        <span className="input-group-text" id="registration-basic-addon1">@</span>
-                        <input type="text" className="form-control" id="registrationUsernameInput" placeholder="Username" aria-label="Username" aria-describedby="registration-basic-addon1 usernameHelp" />
-                    </div>
-                    <small id="usernameHelp" className="form-text text-muted">The username should be between X-Y characters in length.</small>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="registrationPasswordInput"><h5>Password</h5></label>
-                    <input type="password" className="form-control" id="registrationPasswordInput" aria-describedby="passwordHelp" placeholder="Password" />
-                    <small id="passwordHelp" className="form-text text-muted">The password should be between X-Y characters in length.</small>
-                </div>
-                <button type="submit" className="btn btn-primary btn-custom mt-3">Submit</button>
-            </form>
-            <div className="registration-tip">
-            <span>Already registered?</span><br />
-            <a href="/login">Login</a>
+const usernameValidation = (value) => {
+    if (value.trim().length < 1) {
+      return "Username is required";
+    } 
+    if (value.trim().length < 2) {
+      return "Username has to contain at least 2 characters";
+    }
+    if (value.trim().length > 32) {
+        return "Username has to contain less than 32 characters";
+    }
+    return null;
+};
+
+const passwordValidation = (value) => {
+    if (value.trim().length < 1) {
+      return "Password is required";
+    } 
+    if (value.trim().length < 8) {
+      return "Password has to contain at least 8 characters";
+    }
+    return null;
+};
+
+const ErrorAlert = ({ errors }) => {
+    return (
+        <Fragment>
+            <div className="m-2" role="alert">
+                <ul className="list-group">
+                    {errors.map( (error, idx) => <li className="list-group-item list-group-item-danger" key={idx}>{error}</li>)}
+                </ul>
             </div>
-        </div>
-    </Fragment>
-  );
+        </Fragment>
+    );
+}
+
+class RegistrationForm extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: "",
+            password: "",
+            errors: []
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let errors = [];
+        let usernameVal = usernameValidation(this.state.username);
+        let passwordVal = passwordValidation(this.state.password);
+        if (usernameVal) errors.push(usernameVal);
+        if (passwordVal) errors.push(passwordVal);
+
+        this.setState({
+            errors: errors
+        })
+
+        if (errors.length === 0) {
+            const user = { username: this.state.username, password: this.state.password };
+            
+            fetch('http://localhost:5000/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user),
+            })
+            .then(response => response.json())
+            .then(user => {
+                console.log('Success:', user);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+
+        
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div className="registration-container">
+                    <form onSubmit={this.handleSubmit}>
+                        <h1>Register</h1>
+                        {this.state.errors.length > 0 ? <ErrorAlert errors={this.state.errors} /> : null}
+                        <div className="form-group">
+                            <label htmlFor="registrationUsernameInput"><h5>Username</h5></label>
+                            <div className="input-group">
+                                <span className="input-group-text" id="registration-basic-addon1">@</span>
+                                <input type="text" className="form-control" id="registrationUsernameInput" 
+                                        placeholder="Username" aria-label="Username" aria-describedby="registration-basic-addon1 usernameHelp" 
+                                        name="username" value={this.state.username} onChange={this.handleInputChange} 
+                                        minLength="2" maxLength="32" required />
+                            </div>
+                            <small id="usernameHelp" className="form-text text-muted">The username should be between 2-32 characters in length.</small>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="registrationPasswordInput"><h5>Password</h5></label>
+                            <input type="password" className="form-control" id="registrationPasswordInput" aria-describedby="passwordHelp" 
+                                    placeholder="Password" name="password" value={this.state.password} onChange={this.handleInputChange} 
+                                    minLength="8" required />
+                            <small id="passwordHelp" className="form-text text-muted">The password should be at least 8 characters in length.</small>
+                        </div>
+                        <button type="submit" className="btn btn-primary btn-custom mt-3">Submit</button>
+                    </form>
+                    <div className="registration-tip">
+                    <span>Already registered?</span><br />
+                    <a href="/login">Login</a>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
 };
 
 export default RegistrationForm;
