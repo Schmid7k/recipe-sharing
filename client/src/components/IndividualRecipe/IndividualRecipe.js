9b19905b39dd04670e41f72b743c91b0de783671
Annastiina Ahola <anna.s.ahola@gmail.com>
2021-11-17 03:11:01 +0200

    Add component descriptions to IndividualRecipe.js, pass the recipe id to IndividualRecipe component as props, tweak how recipe data is stored in state
    Add component descriptions in comments above components to IndividualRecipe.js. Pass the recipe id as a prop to IndividualRecipe so that it can be accessed through this.props.id for fetching recipe data in the future. Tweak how data is stored in IndividualRecipe state so that only the data is stored and then passed to the components in render instead of storing components itself in the state.

--- client/src/components/IndividualRecipe/IndividualRecipe.js
+++ client/src/components/IndividualRecipe/IndividualRecipe.js
@@ -5,6 +5,12 @@ import bookmarkIcon from "../../images/bookmark_svg.svg";
 import starIcon from "../../images/star_svg.svg";
 import recipeInfo from "../../recipe-placeholder-data.json";
 
+/**
+ * Component for rendering a recipe image.
+ * @param {string} source The source for the image file
+ * @param {string} alternative Alternative text for the image if it doesn't render
+ * @returns A img element styled with .recipe-image that uses the provided source and alternative.
+ */
 const RecipeImage = ({ source, alternative }) => {
     return (
         <Fragment>
@@ -18,6 +24,19 @@ RecipeImage.propTypes = {
     alternative: PropTypes.string.isRequired,
 };
 
+/**
+ * Component for rendering the recipe header that includes the name of the dish, username for the submitter,
+ * recipe category, bookmarks and star rating.
+ * @param {string} name Name of recipe to be displayed
+ * @param {string} author Username of the submitter to be displayed
+ * @param {string} category Category of the recipe to be displayed
+ * @param {number} bookmarks Number of bookmarks for the recipe
+ * @param {number} stars Star rating of the recipe
+ * @returns A .recipe-title div that contains .recipe-title-text and .recipe-title-icons divs.
+ *          .recipe-title-text div contains headers for the name of the dish, the author and the category.
+ *          .recipe-title-icons div contains the bookmark count and star rating and image icons for those 
+ *          displayed next to them.
+ */
 const RecipeHeader = ({ name, author, category, bookmarks, stars }) => {
     return (
       <Fragment>
@@ -46,6 +65,11 @@ RecipeHeader.propTypes = {
     stars: PropTypes.number.isRequired,
 };
 
+/**
+ * Component for rendering a single tag.
+ * @param {string} name Name of the tag to be shown on the tag 
+ * @returns A styled .recipe-tag div with the tag name as content.
+ */
 const RecipeTag = ({ name }) => {
     return (
       <Fragment>
@@ -60,7 +84,33 @@ RecipeTag.propTypes = {
     name: PropTypes.string.isRequired,
 };
 
-const RecipeIngredient = ({ name, amount, unit }) => {
+/**
+ * Component for rendering a list of tags.
+ * @param {Array[string]} tags Array of tags to be rendered
+ * @returns A styled .recipe-tag-list div with RecipeTag elements inside it.
+ */
+const RecipeTagList = ({ tags }) => {
+    return (
+        <Fragment>
+            <div className="recipe-tag-list">
+                {tags.map(tag => <RecipeTag name={tag} key={`${tag}-tag`} />)}
+            </div>
+        </Fragment>
+    );
+};
+
+RecipeTagList.propTypes = {
+    tags: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
+
+/**
+ * Component for rendering a single ingredient.
+ * @param {string} name The name of the ingredient
+ * @param {string} amount The amount of the ingredient including the unit
+ * @returns A styled .recipe-ingredient div with two styled divs inside it, .recipe-ingredient-name div with the name
+ *          as content and .recipe-ingredient-amount with the amount as content.
+ */
+const RecipeIngredient = ({ name, amount }) => {
     return (
       <Fragment>
             <div className="recipe-ingredient">
@@ -68,7 +118,7 @@ const RecipeIngredient = ({ name, amount, unit }) => {
                     {name}
                 </div>
                 <div className="recipe-ingredient-amount">
-                    {amount} {unit}
+                    {amount}
                 </div>
             </div>
       </Fragment>
@@ -78,9 +128,84 @@ const RecipeIngredient = ({ name, amount, unit }) => {
 RecipeIngredient.propTypes = {
     name: PropTypes.string.isRequired,
     amount: PropTypes.string.isRequired,
-    unit: PropTypes.string.isRequired,
 };
 
+/**
+ * Component for rendering a list of ingredients.
+ * @param {string}  group   Name of the current ingredient subgroup
+ * @param {Array[Object]} ingredients Array of ingredients where every element inside it should be of format
+ *                                    { name: 'Ingredient name', amount: 'Ingredient amount' }
+ * @returns A .recipe-ingredients-list div containing RecipeIngredient elements. 
+ *          If the group rendered is not "Default", the group name header is included above the .recipe-ingredients-list div.
+ */
+ const RecipeIngredientList = ({ group, ingredients }) => {
+    return (
+        <Fragment>
+            {group !== "Default" ? <h5>{group}</h5> : null}
+            <div className="recipe-ingredients-list">
+                {ingredients.map(ingredient => 
+                    <RecipeIngredient   name={ingredient.name} amount={ingredient.amount} 
+                                        key={`${group}-ingredient-${ingredient.name}`} />
+                )}
+            </div>
+        </Fragment>
+    );
+};
+
+RecipeIngredientList.propTypes = {
+    group: PropTypes.string.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+    })).isRequired,
+};
+
+/**
+ * Component for rendering a list of ingredient groups.
+ * @param {Array[Object]} ingredient Array of ingredient groups where every element inside it should have the format of
+ *                                   { name: 'Group name', ingredients: [ name: 'Ingredient name', amount: 'Ingredient amount'] }
+ * @returns A .recipe-element div element containing a header and a .recipe-ingredients div.
+ *          .recipe-ingredients div contains RecipeIngredientList elements.
+ */
+ const RecipeIngredientGroups = ({ ingredients }) => {
+    return (
+        <Fragment>
+            <div className="recipe-element">
+                <h3>Ingredients</h3>
+                <div className="recipe-ingredients">
+                    {ingredients.map(group => 
+                        <RecipeIngredientList   group={group.name} 
+                                                ingredients={group.ingredients} key={group.name} />
+                    )}
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+RecipeIngredientGroups.propTypes = {
+    ingredients: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            ingredients: PropTypes.arrayOf(
+                PropTypes.shape({
+                    name: PropTypes.string.isRequired,
+                    amount: PropTypes.string.isRequired,
+                })
+            ).isRequired,
+        })
+    ).isRequired,
+};
+
+
+
+/**
+ * Component for rendering a single instruction.
+ * @param {number} step Step number of the instruction
+ * @param {string} description The description for the current instrucion step
+ * @returns A styled .recipe-instruction div with a header that has the step and a styled 
+ *          .recipe-instruction-description div with the step description as content.
+ */
 const RecipeInstruction = ({ step, description }) => {
     return (
       <Fragment>
@@ -99,6 +224,40 @@ RecipeInstruction.propTypes = {
     description: PropTypes.string.isRequired,
 };
 
+/**
+ * Component for rendering a list of instructions.
+ * @param {Array[Object]} instructions Array of instructions to be rendered
+ * @returns A .recipe-element div with a header and a styled .recipe-instructions-list div containing 
+ *          RecipeInstruction elements inside it.
+ */
+ const RecipeInstructionList = ({ instructions }) => {
+    return (
+        <Fragment>
+            <div className="recipe-element">
+                <h3>Instructions</h3>
+                <div className="recipe-instructions-list">
+                    {instructions.map((instruction, idx) => 
+                        <RecipeInstruction step={instruction.step} description={instruction.instruction} key={idx} />
+                    )}
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+RecipeInstructionList.propTypes = {
+    instructions: PropTypes.arrayOf(PropTypes.shape({
+        step: PropTypes.number.isRequired,
+        instruction: PropTypes.string.isRequired,
+    })).isRequired,
+};
+
+/**
+ * Component for rendering the header 'Additional instructions' and the additional instructions itself
+ * @param {string} description  The content that should be shown under Additional instructions 
+ * @returns A .recipe-element div with a header and a styled .recipe-additional-instructions div containing 
+ *          the content of description prop inside it.
+ */
 const RecipeAdditionalInstructions = ({ description }) => {
     return (
       <Fragment>
@@ -112,95 +271,100 @@ const RecipeAdditionalInstructions = ({ description }) => {
     );
 };
 
+RecipeAdditionalInstructions.propTypes = {
+    description: PropTypes.string.isRequired,
+};
 
+/**
+ * Component to render an individual recipe.
+ * Stores all the relevant recipe data in its state, this.state.
+ * @param   {string}  this.props.id   The recipe of the recipe being rendered by IndividualRecipe
+ * @returns The whole recipe as a component using child components to render the parts inside it.
+ */
 class IndividualRecipe extends React.Component {
     constructor(){
         super();
     
         this.state = {
-            recipe: {}
+            header: {
+                name: "",
+                author: "",
+                category: "",
+                bookmarks: 0,
+                stars: 0
+            },
+            image: {
+                source: "",
+                alternative: "",
+            },
+            additionalInstructions: "",
+            tags: [],
+            ingredients: [],
+            instructions: [],
+            recipe: {
+            }
         }
     }
 
     componentDidMount(){
-        let recipe = {
-            header: <RecipeHeader name={recipeInfo.recipeInfo.title} author={recipeInfo.recipeInfo.username} category={recipeInfo.recipeInfo.category} bookmarks={recipeInfo.recipeInfo.bookmarks} stars={recipeInfo.recipeInfo.stars} />,
-            img: <RecipeImage source={recipeInfo.recipeInfo.image} alternative={recipeInfo.recipeInfo.title} />,
-            additionalInstructions: <RecipeAdditionalInstructions description={recipeInfo.recipeInfo.additionalInstructions} />,
-            tags: [],
-            defaultIngredients: [],
-            subgroups: [],
-            instructions: []
-        };
-
-        recipeInfo.recipeInfo.tags.forEach((tag, idx) => {
-            recipe.tags.push( <RecipeTag name={tag.name} key={idx} /> );
-        });
-
-        recipeInfo.recipeInfo.ingredients.defaultIngredients.forEach((ingredient, idx) => {
-            recipe.defaultIngredients.push( <RecipeIngredient name={ingredient.name} amount={ingredient.amount} unit={ingredient.unit} key={idx} /> );
-        });
-
-        recipeInfo.recipeInfo.ingredients.subgroups.forEach((group, gidx) => {
-            recipe.subgroups.push( 
-                <div key={gidx}>
-                    <h5>{group.name}</h5>
-                    <div className="recipe-ingredients-list">
-                        {group.groupIngredients.map((ingredient, idx) => (<RecipeIngredient name={ingredient.name} amount={ingredient.amount} unit={ingredient.unit} key={idx} />))}
-                    </div>
-                </div>
-            );
-        });
-
-        recipeInfo.recipeInfo.instructions.forEach((instruction, idx) => {
-            recipe.instructions.push( <RecipeInstruction step={instruction.step} description={instruction.description} key={idx} /> );
-        });
-
+        // TODO: get data for the state using this.props.id (=recipe id) instead of using placeholder data
+        //       and store it in the state v
         this.setState({
-            recipe: recipe
-        });
+            header: {
+                name: recipeInfo.recipeInfo.title,
+                author: recipeInfo.recipeInfo.username,
+                category: recipeInfo.recipeInfo.category,
+                bookmarks: recipeInfo.recipeInfo.bookmarks,
+                stars: recipeInfo.recipeInfo.stars
+            },
+            image: {
+                source: recipeInfo.recipeInfo.image,
+                alternative: recipeInfo.recipeInfo.title
+            },
+            additionalInstructions: recipeInfo.recipeInfo.additionalInstructions,
+
+            // assuming recipeInfo.recipeInfo.tags is of format 
+            // tags = ["tag", "tag", "tag"]
+            tags: recipeInfo.recipeInfo.tags,
+
+            // assuming recipeInfo.recipeInfo.ingredients is of format:
+            //  ingredients = [
+            //      { 
+            //          name: "Group name", 
+            //          ingredients: [
+            //              { 
+            //                  name: "Ingredient name", 
+            //                  amount: " Ingredient amount"
+            //              }, 
+            //              ...
+            //          ]
+            //      },
+            //      ...
+            //  ]
+            ingredients: recipeInfo.recipeInfo.ingredients,
+
+            // assuming recipeInfo.recipeInfo.instructions is of format 
+            // instructions = [{step: "1", instruction: "Instruction text"}, {step: "2", instruction: "Instruction text"}]
+            instructions: recipeInfo.recipeInfo.instructions
+        })
     }
 
     render(){
         return (
             <Fragment>
                 <div className="recipe-container">
-
                     <div className="recipe-column-left">
-
-                        {this.state.recipe.img}
-
-                        <div className="recipe-tag-list">
-                            {this.state.recipe.tags}
-                        </div>
-
+                        <RecipeImage source={this.state.image.source} alternative={this.state.image.alternative} />
+                        <RecipeTagList tags={this.state.tags} />
                     </div>
-
                     <div className="recipe-column-right">
-
-                        {this.state.recipe.header}
-
-                        <div className="recipe-element">
-                            <h3>Ingredients</h3>
-                            <div className="recipe-ingredients">
-                                <div className="recipe-ingredients-list">
-                                    {this.state.recipe.defaultIngredients}
-                                </div>
-                                {this.state.recipe.subgroups}
-                            </div>
-                        </div>
-
-                        <div className="recipe-element">
-                            <h3>Instructions</h3>
-                            <div className="recipe-instructions-list">
-                                {this.state.recipe.instructions}
-                            </div>
-                        </div>
-
-                        {this.state.recipe.additionalInstructions}
-
+                        <RecipeHeader   name={this.state.header.name} author={this.state.header.author} 
+                                        category={this.state.header.category} bookmarks={this.state.header.bookmarks} 
+                                        stars={this.state.header.stars} />
+                        <RecipeIngredientGroups ingredients={this.state.ingredients} />
+                        <RecipeInstructionList instructions={this.state.instructions} />
+                        <RecipeAdditionalInstructions description={this.state.additionalInstructions} />
                     </div>
-
                 </div>
             </Fragment>
         );
--- client/src/pages/Recipe.js
+++ client/src/pages/Recipe.js
@@ -1,20 +1,22 @@
 import React, { Fragment } from "react";
+import { useParams } from 'react-router-dom'
 
 //components
 import NavigationBar from "../components/NavBar/NavBar";
 import IndividualRecipe from "../components/IndividualRecipe/IndividualRecipe";
 
 const Recipe = () => {
-  return (
-    <Fragment>
-      <NavigationBar />
-      <div className="container-fluid">
-        <div className="container pt-3">
-            <IndividualRecipe />
+    const { id } = useParams();
+    return (
+        <Fragment>
+        <NavigationBar />
+        <div className="container-fluid">
+            <div className="container pt-3">
+                <IndividualRecipe id={id} />
+            </div>
         </div>
-      </div>
-    </Fragment>
-  );
+        </Fragment>
+    );
 }
 
 export default Recipe;
