836edf3f4cc6e7469bbb87eb59e35e51bf7e9306
IceBlockProduction <iceblockproduction@protonmail.com>
2021-11-18 15:35:27 +0100

    Authentication and requesting single recipe done

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ var upload = multer({ storage: storage });
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(cookie_parser("development"))
+app.use(cookie_parser("development"));
 
 // Routes
 
@@ -76,7 +76,11 @@ app.post("/login", async (req, res) => {
         // User exists
         if (await crypt.compare(password, entry.rows[0].pass)) {
           // Correct password
-          res.status(200).send("Login successful!");
+          res.cookie("authentication", username, {
+            signed: true,
+            maxAge: 2592000000,
+          });
+          res.status(302).redirect("/recipes");
         } else {
           // Wrong password
           res.status(401).send("Wrong password for this username!");
@@ -100,30 +104,34 @@ app.post("/login", async (req, res) => {
 
 app.post("/recipes", upload.single("main"), async (req, res) => {
   try {
-    const main = req.file; // retrieve the main image from the request
-    const { recipe } = req.body; // retrieve the text content
-    const content = JSON.parse(recipe);
-    const { title, category, groups, instructions, addInstructions, tags } =
-      content;
+    const cookie = req.signedCookies.authentication;
+    if (cookie) {
+      const main = req.file; // retrieve the main image from the request
+      const { recipe } = req.body; // retrieve the text content
+      const content = JSON.parse(recipe);
+      const { title, category, groups, instructions, addInstructions, tags } =
+        content;
+      const userID = 1; // FIXME: For development purposes the current originator of every recipe will be the user with userID 1
 
-    const userID = 1; // FIXME: For development purposes the current originator of every recipe will be the user with userID 1
+      const newRecipe = await pool.query(
+        "INSERT INTO recipes (UserID, MainImage, Title, AdditionalInstructions) VALUES($1, $2, $3, $4) RETURNING *",
+        [userID, main.path, title, addInstructions]
+      );
 
-    const newRecipe = await pool.query(
-      "INSERT INTO recipes (UserID, MainImage, Title, AdditionalInstructions) VALUES($1, $2, $3, $4) RETURNING *",
-      [userID, main.path, title, addInstructions]
-    );
+      const recipeID = newRecipe.rows[0].recipeid;
 
-    const recipeID = newRecipe.rows[0].recipeid;
-
-    if (
-      (await saveCategories(category, recipeID)) &&
-      (await saveGroups(groups, recipeID)) &&
-      (await saveInstructions(instructions, recipeID)) &&
-      (await saveTags(tags, recipeID))
-    ) {
-      res.status(201).json(newRecipe.rows[0]);
+      if (
+        (await saveCategories(category, recipeID)) &&
+        (await saveGroups(groups, recipeID)) &&
+        (await saveInstructions(instructions, recipeID)) &&
+        (await saveTags(tags, recipeID))
+      ) {
+        res.status(201).json(newRecipe.rows[0]);
+      } else {
+        res.status(500).send("Something went wrong!");
+      }
     } else {
-      res.status(500).send("Something went wrong!");
+      res.status(302).redirect("/register");
     }
   } catch (err) {
     console.error(err.message);
@@ -150,14 +158,83 @@ app.get("/recipes", async (req, res) => {
 app.get("/recipes/:id", async (req, res) => {
   try {
     const { id } = req.params; // Get the URL parameter
-    const recipe = await pool.query(
-      "SELECT * from recipes WHERE RecipeID = $1",
-      [id]
-    ); // Get recipe from database
+    const base = await pool.query("SELECT * from recipes WHERE RecipeID = $1", [
+      id,
+    ]); // Get recipe from database
 
-    if (recipe.rows[0]) {
+    if (base.rows[0]) {
       // Recipe exists
-      res.status(200).json(recipe.rows[0]);
+      // Recipe template for the response
+      var { recipe } = JSON.parse(`{
+        "recipe": {
+          "title": "",
+          "main": "",
+          "category": "",
+          "groups": {
+          },
+          "instructions": {
+          },
+          "addInstructions": "",
+          "tags": []
+        }
+      }`);
+      // Add title to template
+      recipe.title = base.rows[0].title;
+      // Add path to main image to template
+      recipe.main = base.rows[0].mainimage;
+      // Add additional instructions to template
+      recipe.addInstructions = base.rows[0].additionalinstructions;
+      // Get tags
+      const tags = await pool.query(
+        "SELECT tags.name FROM recipes LEFT JOIN recipe_tags ON recipe_tags.recipeid = recipes.recipeid LEFT JOIN tags ON tags.tagid = recipe_tags.tagid WHERE recipes.recipeid = $1",
+        [id]
+      );
+      // Add tags to template
+      recipe.tags = JSON.parse(JSON.stringify(tags.rows));
+      // Get instructions
+      const instructions = await pool.query(
+        "SELECT recipe_instructions.step, recipe_instructions.instruction, recipe_instructions.instruction_image FROM recipes LEFT JOIN recipe_instructions ON recipe_instructions.recipeid = recipes.recipeid WHERE recipes.recipeid = $1",
+        [id]
+      );
+      // Add instructions to template
+      recipe.instructions = JSON.parse(JSON.stringify(instructions.rows));
+      // Get category
+      const category = await pool.query(
+        "SELECT categories.name FROM recipes LEFT JOIN recipe_categories ON recipe_categories.recipeid = recipes.recipeid LEFT JOIN categories ON categories.categoryid = recipe_categories.categoryid WHERE recipes.recipeid = $1",
+        [id]
+      );
+      // Add category to template
+      recipe.category = category.rows[0].name;
+      // Get ingredient groups
+      const ingredient_groups = await pool.query(
+        "SELECT ingredient_groups.name as group, ingredients.name as ingredient, recipe_ingredients.amount FROM recipes LEFT JOIN ingredient_groups ON ingredient_groups.recipeid = recipes.recipeid LEFT JOIN recipe_ingredients ON recipe_ingredients.groupid = ingredient_groups.groupid LEFT JOIN ingredients ON ingredients.ingredientsid = recipe_ingredients.ingredientsid WHERE recipes.recipeid = $1",
+        [id]
+      );
+      // Every ingredient group contains a list of ingredients related to that group
+      for (var i = 0; i < ingredient_groups.rowCount; i++) {
+        const group = ingredient_groups.rows[i].group;
+        const ingredient = ingredient_groups.rows[i].ingredient;
+        const amount = ingredient_groups.rows[i].amount;
+        // Check if the key of the current group already exists in the template
+        if (recipe.groups[group]) {
+          // Key exists
+          recipe.groups[group].push(
+            JSON.parse(
+              JSON.stringify({ ingedient: ingredient, amount: amount })
+            )
+          );
+        } else {
+          // Key does not exists
+          recipe.groups[group] = [];
+          recipe.groups[group].push(
+            JSON.parse(
+              JSON.stringify({ ingedient: ingredient, amount: amount })
+            )
+          );
+        }
+      }
+      // Send response
+      res.status(200).json(recipe);
     } else {
       // Recipe does not exist
       res.status(404).send("This recipe does not exist!");
@@ -198,7 +275,7 @@ app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
 
-// Helper function that save instructions to the instruction database
+// Helper function that saves instructions to the instruction database
 
 async function saveInstructions(instructions, recipeID) {
   try {
