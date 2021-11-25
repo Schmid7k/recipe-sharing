c9a45e1fc5defdf008a84d916f0c6c0b4f295722
IceBlockProduction <iceblockproduction@protonmail.com>
2021-11-25 18:16:15 +0100

    reverted some changes

--- server/db.js
+++ server/db.js
@@ -1,7 +1,13 @@
 const dotenv = require("dotenv").config();
 const Pool = require("pg").Pool;
 
-const CONNECTION_STRING = process.env.DATABASE_URL || process.env.DEV_URL;
+const CONNECTION_STRING = process.env.DATABASE_URL || {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DEV_IP,
+  port: process.env.DEV_PORT,
+  database: process.env.DB_DATABASE,
+};
 
 const pool = new Pool({
   connectionString: CONNECTION_STRING,
