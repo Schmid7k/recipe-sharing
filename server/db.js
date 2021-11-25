bfdf90b747fd1ea69ca033470ad1ee245ee0e8ad
IceBlockProduction <iceblockproduction@protonmail.com>
2021-11-25 19:26:16 +0100

    DB connectio should work now agian

--- server/db.js
+++ server/db.js
@@ -1,17 +1,22 @@
 const dotenv = require("dotenv").config();
 const Pool = require("pg").Pool;
 
-const CONNECTION_STRING = process.env.DATABASE_URL || {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DEV_IP,
-  port: process.env.DEV_PORT,
-  database: process.env.DB_DATABASE,
-};
+var pool;
 
-const pool = new Pool({
-  connectionString: CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-});
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DEV_IP,
+    port: process.env.DEV_PORT,
+    database: process.env.DB_DATABASE,
+    ssl: { rejectUnauthorized: false },
+  });
+}
 
 module.exports = pool;
